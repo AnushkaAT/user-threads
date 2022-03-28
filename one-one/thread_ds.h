d5cce62e08ce4228e0b02cdae3019cd2f6ca359a
Ashlesha Joshi <joshiav19.comp@coep.ac.in>
2022-03-28 01:42:23 +0000

    Update thread_ds.h

--- one-one/thread_ds.h
+++ one-one/thread_ds.h
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 #include <signal.h>
 
+//Size of stack for a thread
 #define STACK_SIZE (1024*64)
 
 typedef struct thread{
+    //thread id
     int th_id;
+
+    //thread state
     int th_state;
+
+    //pointer to arguments
+    void *args;
+    
     //more fields to be added
 }thread;
 
