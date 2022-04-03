97a92be24fabc0797b7f9f826d4cc142a58bf46b
Ashlesha Joshi <joshiav19.comp@coep.ac.in>
2022-04-03 16:04:33 +0000

    Update thread_ds.h

--- one-one/thread_ds.h
+++ one-one/thread_ds.h
@@ -5,6 +5,13 @@
 //Size of stack for a thread
 #define STACK_SIZE (1024*64)
 
+//Maximum threads for each process
+#define MAX_THREADS 75
+
+//thread states
+#define JOINABLE 1
+#define JOINED 2
+
 typedef struct thread{
     //thread id
     int th_id;
@@ -21,7 +28,7 @@ typedef struct thread{
     //stack
     void *stack;
     
-    //retrun value
+    //return value
     void *retval;
     
     //more fields to be added
