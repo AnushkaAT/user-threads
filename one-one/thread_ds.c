e52ad84134cf440b888c942f01c1a8b5b02d8e7c
Ashlesha Joshi <joshiav19.comp@coep.ac.in>
2022-04-14 17:01:15 +0000

    Added test code for small part(one-one)

--- one-one/thread_ds.c
+++ one-one/thread_ds.c
@@ -1,7 +1,8 @@
 #include "thread_ds.h"
+#include <stdio.h>
 
 //inserting node at the beginning of the list
-void insert(node **front, thread new){
+void insert(node **front, thread *new){
     node *n;
     n = (node *)(malloc(sizeof(node)));
     n->block = new;
@@ -26,7 +27,7 @@ node* searchtid(node *front, int threadid){
 
     while(n != NULL){
         
-        if(n->block.th_id == threadid){
+        if(n->block->th_id == threadid){
             return n;
         }
         n = n->next;
@@ -37,17 +38,18 @@ node* searchtid(node *front, int threadid){
 
 }
 
-node* remove(node **front, int threadid){
+//removing a node with given thread id
+node* removenode(node **front, int threadid){
     node *n = *front;
 
-    if(n != NULL && n->block.th_id == threadid){
+    if(n != NULL && n->block->th_id == threadid){
         *front = n->next;
         //free(n);
         return n;
     }
 
     node *p = NULL;
-    while(n != NULL && n->block.th_id != threadid){
+    while(n != NULL && n->block->th_id != threadid){
         p = n;
         n = n->next;
     }
@@ -62,3 +64,37 @@ node* remove(node **front, int threadid){
     return n;
 }
 
+//Traverse the linked list
+void traverse(node *front){
+    node *n;
+    n = front;
+    if(n == NULL){
+        printf("The List Is Empty\n");
+        return;
+    }
+
+    //traverse through the entire list
+    while(n != NULL){
+        printf("Thread Id: %d\n", n->block->th_id);
+        n = n->next;
+    }
+    return;
+}
+
+int main(){
+    thread *t1;
+    thread *t2;
+    thread *t3;
+    t1 = (thread *)(malloc(sizeof(thread)));
+    t2 = (thread *)(malloc(sizeof(thread)));
+    t3 = (thread *)(malloc(sizeof(thread)));
+    node *n1;
+    n1 = NULL;
+    t1->th_id = 100;
+    t2->th_id = 400;
+    t3->th_id = 2;
+    insert(&n1, t1);
+    insert(&n1, t2);
+    insert(&n1, t3);
+    traverse(n1);
+}
