26af677d670f8c707bcdefc7e1b2245275e1294f
Ashlesha Joshi <joshiav19.comp@coep.ac.in>
2022-05-01 09:17:55 +0000

    one-one test code 1

---
+++ one-one/test1.c
@@ -0,0 +1,46 @@
+#include "thread.c"
+
+//Fibonacci Series
+void* fibnum(void *y){
+	printf("Inside Thread1\n");
+	int i = 0, j = 1, k;
+	printf("The Fibonacci series is: ");
+	printf("%d %d", i, j);
+	for(int n = 2;n < 10; n++){
+		k = i + j;
+		printf(" %d", k);
+		i = j;
+		j = k;
+	}
+}
+
+//Matrix Addition
+void* matadd(void *z){
+	printf("\nInside Thread2!\n");
+	int a[4][4] = {{1,2,3,4}, {5,6,7,8}, {1,2,3,4}, {5,6,7,8}};
+	int b[4][4] = {{1,2,3,4}, {5,6,7,8}, {1,2,3,4}, {5,6,7,8}};
+	int c[4][4] = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
+	for(int i = 0;i < 4;i++){
+		for(int j = 0;j < 4;j++){
+			c[i][j] = a[i][j] + b[i][j];
+		}
+	}
+	
+	for(int i = 0;i < 4;i++){
+		for(int j = 0;j < 4;j++){
+			printf("%d\t", c[i][j]);
+		}	
+		printf("\n");
+	}
+}
+
+int main(){
+	thread *t1, *t2;
+	thread_create(t1, fibnum, NULL);
+	thread_create(t2, matadd, NULL);
+	n->block->th_state = JOINABLE;
+	thread_join(*n->block, NULL);
+	int x = 6;
+	thread_exit(&x);
+	return 0;
+	}
